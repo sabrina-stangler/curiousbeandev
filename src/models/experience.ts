b0436dec4e4dedd2623fc71ba6b2550da4356a9a
Sabrina Stangler <sabrinastangler51@gmail.com>
2023-02-09 20:04:46 -0600

    fill out experience strings

--- src/models/experience.ts
+++ src/models/experience.ts
@@ -11,11 +11,17 @@ export enum ProjectAttr {
   ui_ux = 'ui/ux design'
 }
 
+export interface ProjectLink {
+  label: string;
+  href: string
+}
+
 export interface Project {
   title: string
   description: string
   bullets: string[]
   attrs: ProjectAttr[]
+  links?: ProjectLink[]
 }
 
 export interface Organization {

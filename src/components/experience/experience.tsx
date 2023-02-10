9b8e29e2a98f3ee433cb0095f85591146b7714cd
Sabrina Stangler <sabrinastangler51@gmail.com>
2023-02-10 14:04:03 -0600

    format dates on timeline given start date

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,23 +1,28 @@
 import './experience.css'
 import { ProjectCard } from './components/project'
 import { organizations } from '../../constants'
+import { useCallback } from 'react'
 
 export function Experience() {
+  
+  const getFormatedDate = useCallback((d: Date) => {
+    return d.toLocaleDateString('en-us', { year:"numeric", month:"short"}) 
+  }, [])
 
   return (
     <div className="experience-container">
       <h1>Work Experience</h1>
       <div className="timeline">
-        <h2 className="year-label">- 2023</h2>
-        {organizations.map(o => (
+        <h2 className="year-label">2023</h2>
+        {organizations.map(o => (<>
           <div className="company-container">
             <h3 className="company-label">{o.name}</h3>
             <div className="company-project-container">
               {o.projects.map(p => <ProjectCard project={p} />)}
             </div>
           </div>
-        ))}
-        <h2 className="year-label">- 2016</h2>
+          <h2 className="year-label">{getFormatedDate(o.startDate)}</h2>
+        </>))}
       </div>
     </div>
   )

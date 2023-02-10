b29518f591c2cc8a351148201ff8533d1dd9022d
Sabrina Stangler <sabrinastangler51@gmail.com>
2023-02-10 13:51:56 -0600

    stylize project tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
 import { Experience, Footer, Logo } from './components';
+import {Helmet} from 'react-helmet'
+
+import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <Helmet>
+        <script src="https://kit.fontawesome.com/947016a734.js" crossOrigin="anonymous"></script>
+      </Helmet>
       <div className="header">
         <div className="logo-container">
           <Logo />
--- src/components/experience/components/project.tsx
+++ src/components/experience/components/project.tsx
@@ -1,5 +1,6 @@
 import { Project } from '../../../models'
 import './project.css'
+import { ProjectTags } from './tags'
 
 export interface ProjectProps {
   project: Project
@@ -9,27 +10,29 @@ export function ProjectCard({ project }: ProjectProps) {
   const { title, description, bullets, attrs, links } = project
   return (
     <div className="project-container">
-      <span className="project-title">{title}</span>
-      <span className="project-description">{description}</span>
-      <div className="project-bullets">
-        {bullets && bullets.length > 0 &&
-          <ul className="project-bullets-ul">
-            {bullets.map(b => <li>{b}</li>)}
-          </ul>}
+      <div className="project-content">
+        <span className="project-title">{title}</span>
+        <span className="project-description">{description}</span>
+        <div className="project-bullets">
+          {bullets && bullets.length > 0 &&
+            <ul className="project-bullets-ul">
+              {bullets.map(b => <li>{b}</li>)}
+            </ul>}
+        </div>
+        {links && <div className="project-links">
+          {links.map((l, i) => {
+            let index = ''
+            for(let j = 0; j<=i; j++) {
+              index = index.concat('!')
+            }
+            return (
+              <a href={l.href}>{index} {l.label}</a>
+            )
+          })}
+        </div>}
       </div>
-      {links && <div className="project-links">
-        {links.map((l, i) => {
-          let index = ''
-          for(let j = 0; j<=i; j++) {
-            index = index.concat('!')
-          }
-          return (
-            <a href={l.href}>{index} {l.label}</a>
-          )
-        })}
-      </div>}
       <div className="project-attrs">
-        {attrs.sort((a,b) => a.localeCompare(b)).join(', ')}
+        <ProjectTags tags={attrs} />
       </div>
     </div>
   )

e132d85eaf32e1764f13d0d2b9776fa70010dec8
Sabrina Stangler <sabrinastangler51@gmail.com>
2023-02-09 20:04:59 -0600

    minor project restyling

--- src/components/experience/project.tsx
+++ src/components/experience/project.tsx
@@ -6,7 +6,7 @@ export interface ProjectProps {
 }
 
 export function ProjectCard({ project }: ProjectProps) {
-  const { title, description, bullets, attrs } = project
+  const { title, description, bullets, attrs, links } = project
   return (
     <div className="project-container">
       <span className="project-title">{title}</span>
@@ -17,6 +17,17 @@ export function ProjectCard({ project }: ProjectProps) {
             {bullets.map(b => <li>{b}</li>)}
           </ul>}
       </div>
+      {links && <div className="project-links">
+        {links.map((l, i) => {
+          let index = ''
+          for(let j = 0; j<=i; j++) {
+            index = index.concat('!')
+          }
+          return (
+            <a href={l.href}>{index} {l.label}</a>
+          )
+        })}
+      </div>}
       <div className="project-attrs">
         {attrs.sort((a,b) => a.localeCompare(b)).join(', ')}
       </div>

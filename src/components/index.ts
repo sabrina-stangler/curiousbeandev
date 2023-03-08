848e80450be02f2af7710c3ee192146311b03150
Sabrina Stangler <sabrinastangler51@gmail.com>
2023-03-08 14:37:01 -0600

    profile section: oops i just did the whole thing

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './experience'
 export * from './footer'
 export * from './logo'
+export * from './profile'
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
+export * from './bio'
 export * from './leadership-experience'
 export * from './product-experience'

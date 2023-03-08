766ed5682769d3737fb43fa6e49e5bd2223683bf
Sabrina Stangler <sabrinastangler51@gmail.com>
2023-03-08 14:48:37 -0600

    minor restyling and remove two pictures

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -5,7 +5,7 @@ import Graduation from '../../assets/images/me/me-collin-graduation.jpg'
 import SelfiePurple from '../../assets/images/me/selfie-purple-head-square.jpg'
 import SXSWTrio from '../../assets/images/me/sxsw-trio.jpg'
 import SelfieLolla from '../../assets/images/me/selfie-lolla-blue.jpg'
-import { NAME, BIO_PARAGRAPHS, SELF_TAGLINE } from '../../constants'
+import { NAME, BIO_PARAGRAPHS } from '../../constants'
 
 export function Profile() {
 
@@ -17,15 +17,15 @@ export function Profile() {
         </div>
         <div className="profile-info-container">
           <div className="profile-name">{NAME}</div>
-          <div className="profile-tagline">{SELF_TAGLINE}</div>
-          <div className="profile-bio">{BIO_PARAGRAPHS.map(line => <p>{line}</p>)}</div>
+          <div className="profile-tagline">#README</div>
+          <div className="profile-bio">{BIO_PARAGRAPHS.map(line => <div>{line}</div>)}</div>
         </div>
       </div>
       <div className="profile-gallery">
-        <img src={Graduation} alt="graduation with my partner collin" className="profile-gallery-photo-img" />
+        <img src={Graduation} alt="graduation from MSOE with my partner collin" className="profile-gallery-photo-img" />
         <img src={RBG} alt="halloween costume as ruth bader ginsburg (rbg) with a hammer as a gavel" className="profile-gallery-photo-img" />
-        <img src={SelfieLolla} alt="selfie from lollapalooza music festival with graphic eyeliner and face glitter" className="profile-gallery-photo-img" />
-        <img src={CaliStuntin} alt="me posing with hands on hips and hips tilted away from the camera laterally, i'm slaying" className="profile-gallery-photo-img" />
+        {/* <img src={SelfieLolla} alt="selfie from lollapalooza music festival with graphic eyeliner and face glitter" className="profile-gallery-photo-img" />
+        <img src={CaliStuntin} alt="me posing with hands on hips and hips tilted away from the camera laterally, i'm slaying" className="profile-gallery-photo-img" /> */}
         <img src={SXSWTrio} alt="me and my uif cohort at south by southwest (sxsw) in 2018" className="profile-gallery-photo-img" />
       </div>
     </div>
